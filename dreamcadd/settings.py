c628c203d69db0cc0e65c2508ae032a8504a434d
purushggl-design <purushggl@gmail.com>
2026-09-15 11:01:49 +0530

    Configure WhiteNoise and dj-database-url for Railway deployment

--- dreamcadd/settings.py
+++ dreamcadd/settings.py
@@ -81,6 +81,7 @@ MESSAGE_TAGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,11 +112,22 @@ WSGI_APPLICATION = 'dreamcadd.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-# Defaults to SQLite for quick local dev. Set DB_ENGINE=postgres (+ the vars
-# below) to point this at the PostgreSQL database from dreamcadd_schema.sql.
+# Supports DATABASE_URL (Railway Postgres), DB_ENGINE=postgres, or fallback SQLite
+if os.environ.get('DATABASE_URL'):
+    try:
+        import dj_database_url
+        DATABASES = {
+            'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
+        }
+    except ImportError:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+elif os.environ.get('DB_ENGINE') == 'postgres':
 
-if os.environ.get('DB_ENGINE') == 'postgres':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -187,9 +199,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
